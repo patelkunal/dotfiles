1dfa66053c5d4c238adaa36a022f45f82142f6d9
kppatel <patelkunal89@gmail.com>
2014-08-31 14:17:28 +0530

    adding /tmp/ folder path for wget

--- python/pre-reqz.sh
+++ python/pre-reqz.sh
@@ -1,8 +1,8 @@
-wget http://python-distribute.org/distribute_setup.py
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+wget http://python-distribute.org/distribute_setup.py -P /tmp/
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -P /tmp/
 
-sudo python distribute_setup.py
-sudo python get-pip.py
+sudo python /tmp/distribute_setup.py
+sudo python /tmp/get-pip.py
 
 sudo pip install setuptools
 
