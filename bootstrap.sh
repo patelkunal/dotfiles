bd8c48df66eb71ea4d53ec54e188114337bf740a
kppatel <kppatel@verisign.com>
2015-04-27 10:03:10 +0530

    added steps in comment for auto-install

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+# for every packages, follow following steps. please add comments to mention
+# about step
+#	1. Fetch required archived version from web or use yum/apt-get
+#	2. prepare destination directory and apply permissions and stuffs
+#	3. extract archive to destination
+#	4. prepare PATH for executables.
+#	5. test everything is in place or not
