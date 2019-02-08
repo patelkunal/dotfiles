95ebceb4e1d8a18ac277172c6ebb7524c0a7a704
Kunal Patel <kupa@microsoft.com>
2019-02-08 20:29:15 +0530

    visual studio aliases and faster git

--- aliases.bat
+++ aliases.bat
@@ -10,3 +10,7 @@ doskey grep=find "$1" $2
 doskey mv=ren $*
 doskey rm=del $*
 doskey touch=fsutil file createNew $1 0
+doskey ps=PowerShell $*
+doskey vsc="\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" $*
+doskey vse="\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv" $*
+doskey vs="\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv" $*
